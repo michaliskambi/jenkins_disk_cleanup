e2dfe0ec3518d59860e16a149257ab49fca66a7e
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-10-12 00:08:25 +0200

    Ignore killall result, to continue when jenkins_disk_cleanup process was not running, and it was not necessary to kill it

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ castle-engine compile --mode=debug # no need to make optimized build for this
 #   dest open "/home/michalis/bin/jenkins_disk_cleanup": Failure
 #   scp: failed to upload file jenkins_disk_cleanup to /home/michalis/bin/jenkins_disk_cleanup
 ssh michalis@jenkins.castle-engine.io << EOF
-killall jenkins_disk_cleanup
+killall jenkins_disk_cleanup || true
 EOF
 
 scp jenkins_disk_cleanup michalis@jenkins.castle-engine.io:/home/michalis/bin/jenkins_disk_cleanup
