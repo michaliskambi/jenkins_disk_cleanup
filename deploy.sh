c212a2f43c0443863a98959bc1ad02d3b500b0bd
Michalis Kamburelis <michalis.kambi@gmail.com>
2023-10-11 23:59:54 +0200

    Make sure you can deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ IFS=$'\n\t'
 
 castle-engine compile --mode=debug # no need to make optimized build for this
 
+# Trying to do scp when it is running ends with
+#   dest open "/home/michalis/bin/jenkins_disk_cleanup": Failure
+#   scp: failed to upload file jenkins_disk_cleanup to /home/michalis/bin/jenkins_disk_cleanup
+ssh michalis@jenkins.castle-engine.io << EOF
+killall jenkins_disk_cleanup
+EOF
+
 scp jenkins_disk_cleanup michalis@jenkins.castle-engine.io:/home/michalis/bin/jenkins_disk_cleanup
 
 # ssh michalis@jenkins.castle-engine.io << EOF
